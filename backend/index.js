3a3c6a28fea1387a9eb898d7f7744c061ddf7521
GiampaoloResendePereira <giampaolopereira29@gmail.com>
2024-11-25 09:48:48 -0300

    server

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const { createName } = require('./controllers/controller');
+const { createUser } = require('./controllers/UserController');
+const { getAllNames, getAllUsers } = require('./controllers/TabelaController');
+const { updateNameById } = require('./controllers/EditNameController');
+const { deleteNameById } = require('./controllers/DeleteNameController');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'novo_usuario',
-  password: 'nova_senha',
-  database: 'seu_banco'
-});
-
-
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado ao banco de dados MySQL');
-});
-
-app.post('/api/names', (req, res) => {
-  const { name } = req.body;
-  const sql = 'INSERT INTO names (name) VALUES (?)';
-  db.query(sql, [name], (err, result) => {
-    if (err) throw err;
-    res.send('Nome cadastrado com sucesso!');
-  });
-});
+app.post('/api/names', createName);
+app.post('/api/users', createUser);
+app.get('/api/names', getAllNames);
+app.get('/api/users', getAllUsers);
+app.put('/api/names', updateNameById);
+app.delete('/api/names', deleteNameById);
 
 app.listen(5000, () => {
   console.log('Servidor rodando na porta 5000');
 });
+
+//npm init -y
+//npm install express
+//npm install cors
+//npm install body-parser
+//npm install mysql2
+//npm install -g nodemon
