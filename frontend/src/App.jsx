58d111fd538d272dc2d12c75b93bd304eaa3eeb1
GiampaoloResendePereira <giampaolopereira29@gmail.com>
2024-11-25 16:22:32 -0300

    client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NameForm from './components/pages/NameForm';
-import UserForm from './components/pages/UserForm';
-import TabelaForm from './components/pages/TabelaForm';
-import EditNameForm from './components/pages/EditNameForm';
-import DeleteNameForm from './components/pages/DeleteNameForm';
+import NameForm from './components/crud/NameForm';
+import UserForm from './components/crud/UserForm';
+import TabelaForm from './components/crud/TabelaForm';
+import EditNameForm from './components/crud/EditNameForm';
+import DeleteNameForm from './components/crud/DeleteNameForm';
+import CadastroCliente from './components/Formularios/CadastroCliente';
+import EditarParametro from './components/pages/EditarParametro';
+import CalculoFrete from './components/pages/CalculoFrete';
+import SolicitacaoFrete from './components/Formularios/SolicitacaoFrete';
+
 
 function App() {
   return (
@@ -16,7 +21,16 @@ function App() {
           <Route path="/name" element={<NameForm />} />
           <Route path="/tabela" element={<TabelaForm />} />  
           <Route path="/editar" element={<EditNameForm />} />   
-          <Route path="/deletar" element={<DeleteNameForm />} />                        
+          <Route path="/deletar" element={<DeleteNameForm />} /> 
+
+          {/* Formularios */}
+          <Route path="/cadastro-cliente" element={<CadastroCliente />} />
+          <Route path="/solicitacao-frete" element={<SolicitacaoFrete />} />
+
+          {/* Pages */}
+          <Route path="/editar-parametro" element={<EditarParametro />} />
+          <Route path="/calculo-frete" element={<CalculoFrete />} />
+                                 
         </Routes>
       </Router>
     
